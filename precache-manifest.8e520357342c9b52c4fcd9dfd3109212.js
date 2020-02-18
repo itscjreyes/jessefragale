09c98ca61e1c0259b0ff2757fae72c4b24fbbeda
CJ Reyes <itscjreyes@gmail.com>
2020-02-18 14:45:05 -0500

    Updates

---
+++ precache-manifest.8e520357342c9b52c4fcd9dfd3109212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783486bbece2d84656cf80e1ef5bb92f",
+    "url": "/jessefragale/index.html"
+  },
+  {
+    "revision": "eb517e9348cec045396c",
+    "url": "/jessefragale/static/css/main.c6d2e923.chunk.css"
+  },
+  {
+    "revision": "edc85060bb6d445c1964",
+    "url": "/jessefragale/static/js/2.2f7be370.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jessefragale/static/js/2.2f7be370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb517e9348cec045396c",
+    "url": "/jessefragale/static/js/main.cfa0f9da.chunk.js"
+  },
+  {
+    "revision": "451660d2f1845b5e0d51",
+    "url": "/jessefragale/static/js/runtime-main.0b6cb369.js"
+  }
+]);
