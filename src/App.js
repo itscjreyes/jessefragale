c9950e9aa2b46c4786bf3dbffcea549abc7cc3c6
CJ Reyes <itscjreyes@gmail.com>
2020-03-11 07:47:38 -0400

    Banner styled for desktop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import butter from './butter-client';
 import Header from './Components/Header/header.component';
 import Banner from './Components/Banner/banner.component';
+import About from './Components/About/about.component';
 import Media from './Components/Media/media.component';
 import Contact from './Components/Contact/contact.component';
 import Footer from './Components/Footer/footer.component';
 import './Elements/elements-list.component';
-import './App.css';
-import About from './Components/About/about.component';
+import './App.scss';
 
 class App extends Component {
   state = {
@@ -27,10 +27,11 @@ class App extends Component {
   }
   
   render() {
-    const about_img = this.state.data.fields.about_me_image;
-    const about = this.state.data.fields.about_me;
-    const articles = this.state.data.fields.articles;
-    const featured = this.state.data.fields.featured_work;
+    const fields = this.state.data.fields;
+    const about_img = fields.about_me_image;
+    const about = fields.about_me;
+    const articles = fields.articles;
+    const featured = fields.featured_work;
     
     return (
       <div>
