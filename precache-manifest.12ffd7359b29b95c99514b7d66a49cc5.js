814a78d0fc8a46322c214fe58a05ff7f0a820abb
CJ Reyes <itscjreyes@gmail.com>
2020-03-12 08:53:52 -0400

    Updates

---
+++ precache-manifest.12ffd7359b29b95c99514b7d66a49cc5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3469f5028b3945057d1c5e15886021",
+    "url": "/jessefragale/index.html"
+  },
+  {
+    "revision": "a4c4486b692b15d63a4c",
+    "url": "/jessefragale/static/css/main.6e74ee8b.chunk.css"
+  },
+  {
+    "revision": "9f0296836a4873aa14d2",
+    "url": "/jessefragale/static/js/2.b80052fa.chunk.js"
+  },
+  {
+    "revision": "333d194533c647274c30c06414f74d88",
+    "url": "/jessefragale/static/js/2.b80052fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c4486b692b15d63a4c",
+    "url": "/jessefragale/static/js/main.f4b665a6.chunk.js"
+  },
+  {
+    "revision": "451660d2f1845b5e0d51",
+    "url": "/jessefragale/static/js/runtime-main.0b6cb369.js"
+  },
+  {
+    "revision": "37e34810147cf77348f8626b49b2dce8",
+    "url": "/jessefragale/static/media/buttercms-logo.37e34810.png"
+  },
+  {
+    "revision": "82189696539164bc0b0e271a3bcac397",
+    "url": "/jessefragale/static/media/envelope.82189696.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/jessefragale/static/media/instagram.e75317fa.svg"
+  },
+  {
+    "revision": "e8018d1ffc448f68ffbe789af3227c67",
+    "url": "/jessefragale/static/media/jesse-fragale.e8018d1f.png"
+  },
+  {
+    "revision": "bbec9a577aecfe567115fc6ff6d58a1d",
+    "url": "/jessefragale/static/media/jessefragale.bbec9a57.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/jessefragale/static/media/linkedin.1e01f0c0.svg"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/jessefragale/static/media/play.f79fe6ae.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/jessefragale/static/media/twitter.6620c8ae.svg"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/jessefragale/static/media/youtube.c80ca203.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jessefragale/precache-manifest.1e7857926bd97f8566e53c62ff94e030.js"
+  "/jessefragale/precache-manifest.12ffd7359b29b95c99514b7d66a49cc5.js"
 );
 
 self.addEventListener('message', (event) => {
