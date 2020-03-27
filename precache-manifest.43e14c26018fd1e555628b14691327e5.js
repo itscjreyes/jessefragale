bfee2aa2c01780f89b90b5760cbf6c35f787bef1
CJ Reyes <itscjreyes@gmail.com>
2020-03-27 08:55:41 -0400

    Updates

---
+++ precache-manifest.43e14c26018fd1e555628b14691327e5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec8f10cf9eb3d65d40a780877e90e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be2877441e833c60fc2",
+    "url": "/static/css/main.bb6dbc43.chunk.css"
+  },
+  {
+    "revision": "64f2ee63143ef720bfb9",
+    "url": "/static/js/2.a5b9bd24.chunk.js"
+  },
+  {
+    "revision": "333d194533c647274c30c06414f74d88",
+    "url": "/static/js/2.a5b9bd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be2877441e833c60fc2",
+    "url": "/static/js/main.33cac01d.chunk.js"
+  },
+  {
+    "revision": "b819d8bb24bad4b0422a",
+    "url": "/static/js/runtime-main.4092f654.js"
+  },
+  {
+    "revision": "37e34810147cf77348f8626b49b2dce8",
+    "url": "/static/media/buttercms-logo.37e34810.png"
+  },
+  {
+    "revision": "82189696539164bc0b0e271a3bcac397",
+    "url": "/static/media/envelope.82189696.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/static/media/instagram.e75317fa.svg"
+  },
+  {
+    "revision": "e8018d1ffc448f68ffbe789af3227c67",
+    "url": "/static/media/jesse-fragale.e8018d1f.png"
+  },
+  {
+    "revision": "bbec9a577aecfe567115fc6ff6d58a1d",
+    "url": "/static/media/jessefragale.bbec9a57.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/static/media/linkedin.1e01f0c0.svg"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/static/media/play.f79fe6ae.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/static/media/twitter.6620c8ae.svg"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/static/media/youtube.c80ca203.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jessefragale/precache-manifest.ce46e92992d6b062db25611414dcf855.js"
+  "/precache-manifest.43e14c26018fd1e555628b14691327e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jessefragale/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
