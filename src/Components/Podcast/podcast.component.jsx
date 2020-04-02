07e9af99dd1cfbb8a9195fe360763c7f977b931c
CJ Reyes <itscjreyes@gmail.com>
2020-04-02 09:51:31 -0400

    New podcast embed

--- src/Components/Podcast/podcast.component.jsx
+++ src/Components/Podcast/podcast.component.jsx
@@ -5,7 +5,8 @@ import './podcast.styles.scss';
 function Podcast(){
   return (
     <div className="podcast-container">
-        <iframe title="The Working Capital Real Estate Podcast" src="//html5-player.libsyn.com/embed/destination/id/1789700/height/360/theme/legacy/thumbnail/yes/direction/backward/no-cache/true/" height="360" width="100%" scrolling="no"  allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+        <iframe title="The Working Capital Real Estate Podcast" src="///html5-player.libsyn.com/embed/destination/id/1789700/height/360/theme/custom/thumbnail/yes/direction/backward/no-cache/true/render-playlist/yes/custom-color/87A93A/" height="auto" width="100%" scrolling="no"  allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+        {/* <iframe style="border: none" src="//html5-player.libsyn.com/embed/destination/id/1789700/height/360/theme/custom/thumbnail/yes/direction/backward/no-cache/true/render-playlist/yes/custom-color/87A93A/" height="360" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe> */}
     </div>
   )
 }
